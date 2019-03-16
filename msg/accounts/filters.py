c165519292ca2973bf113a58d1307a75d5f3abea
roman47 <dillonginley@gmail.com>
2019-03-16 18:08:37 -0400

    First submission

---
+++ msg/accounts/filters.py
@@ -0,0 +1,10 @@
+import django_filters
+from . import models
+
+
+class ProjectFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = models.Project
+        fields = ['name']
